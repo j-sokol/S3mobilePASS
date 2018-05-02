a347d35a18b7b3cad5d75a9818fa88add122bcce
Jan Sokol <jan.sokol.glx@gmail.com>
2018-05-02 17:39:42 +0200

    Update prepare script

--- prepare-android.sh
+++ prepare-android.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+
+
+# First install Termux on android, then run this script
 pkg install python2 -y
 pip2 install awscli --upgrade --user
 python2 setup.py install
 
+touch ~/.bashrc
 
+
+WRAPPER_FILE=`realpath passer-wrapper-android.sh`
+
+echo "alias pass=\"bash $WRAPPER_FILE\"" >> ~/.bashrc
