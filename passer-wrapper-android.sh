299277dc1f4a0f95e4ddb5b7d82d2bf670f28628
Your Name <jan.sokol.glx@gmail.com>
2018-05-02 17:31:13 +0200

    python2 for android script

--- passer-wrapper-android.sh
+++ passer-wrapper-android.sh
@@ -9,7 +9,7 @@ aws s3 cp s3://${BUCKET_NAME}/${CONF_FILE} ${CONF_PATH}/${CONF_FILE} > ${LOG}
 
 cat "${CONF_PATH}/${CONF_FILE}" >/dev/null 
 
-/data/data/com.termux/files/home/mobilePASSer/mobilepasser/mobilepasser.py 
+python2 /data/data/com.termux/files/home/mobilePASSer/mobilepasser/mobilepasser.py 
 
 
 aws s3 cp "${CONF_PATH}/${CONF_FILE}" "s3://${BUCKET_NAME}/${CONF_FILE}" > ${LOG}
